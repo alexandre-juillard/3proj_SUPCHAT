4b0c6d3456b709e3aab6954398e349ff4dcb0b61
Alexandre Juillard <ajuillard69@gmail.com>
2025-06-13 00:42:18 +0200

    fusion admin avec super_admin

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -84,7 +84,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'admin', 'super_admin'],
+    enum: ['user', 'admin'],
     default: 'user'
   },
   isVerified: {
